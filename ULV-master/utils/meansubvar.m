b0ab8daef7aa307abe1851eee60f9dec017d8587
Hou-Guojia <69958292+Hou-Guojia@users.noreply.github.com>
2023-11-09 20:14:12 +0800

    Delete ULV-master directory

--- ULV-master/utils/meansubvar.m
+++
@@ -1,9 +0,0 @@
-function [q]= meansubvar(I,r)
-[hei, wid] = size(I);
-N = boxfilter(ones(hei, wid), r); % the size of each local patch; N=(2r+1)^2 except for boundary pixels.
-mean_I = boxfilter(I, r) ./ N;
-mean_II = boxfilter(I.*I, r) ./ N;
-var_I = mean_II - mean_I .* mean_I;
-a = mean_I-var_I; % Eqn. (8) in the paper;
-q=boxfilter(var_I, r) ./ N;
-end
